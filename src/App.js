067970eee31b455737fc3a0641821e41b366e88c
Seth Fenske <fenskesd@gmail.com>
2020-03-29 13:52:10 -0500

    Did some stuff

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {PayPalButton} from 'react-paypal-button-v2';
+import {Col, Row} from 'react-flexbox-grid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <PayPalButton/>
+        <Row className='max-width'>
+          <Col id='dashboard' className='curved-border'>
+            Hello there.
+          </Col>
+          <Col id='main-content' className='curved-border'>
+            My name is! Who?
+          </Col>
+          <Col id='other-projects' className='curved-border'>
+            Slim Shady
+          </Col>
+        </Row>
+
+      <PayPalButton
+        client-id="sb"
+        amount = "5.00"
+        currency = "USD"
+      />
       </header>
     </div>
   );
