5551749416065866f7e055063cb197c67c0676ec
Seth Fenske <fenskesd@msoe.edu>
2020-04-14 16:31:00 -0500

    Fixed vertical scaling for the tasks

--- src/App.js
+++ src/App.js
@@ -7,33 +7,31 @@ import ProjectCard from './Project Card/ProjectCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Row className='max-width height-fill scroll' >
-          <Col id='dashboard' className="curved-border" style={{height: "100%"}}>
-            <div className="colum-main-content">
+    <div className="App height-fill">
+      <header className="App-header height-fill">
+        <Row className='max-width height-fill' >
+          <Col id='dashboard' className="curved-border" >
+            <div>
                 <h1>Account</h1>
             </div>
           </Col>
-          <Col id='main-content' className="curved-border" style={{height: "100%"}}>
+          <Col id='main-content' className="curved-border" >
             <div className="header">
                 <h1>Details</h1>
             </div>
             <div className='colum-main-conent'>My name is! Who?</div>
           </Col>
-          <Col id='other-projects' className="curved-border" style={{height: "100%"}}>
-            <div style={{height: "95%"}}>
-              <div className="header">
-                <h1>Projects</h1>
-              </div>
-              <div className='curved-border scroll' style={{height: "85%"}}>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
+          <Col id='other-projects' className="curved-border" >
+            <div className="header">
+              <h1>Projects</h1>
             </div>
+            <div id='project-cards' className='curved-border scroll'>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
             </div>
           </Col>
         </Row>
